32884f3d1647aaed05070f1b8e2959e55f9c9c1f
Ravi Chepuri <rchepuri99@gmail.com>
2024-05-12 01:53:50 -0400

    Quick edits

--- examples/simple_example_lorenz.py
+++ examples/simple_example_lorenz.py
@@ -9,8 +9,8 @@ t_pred, actual, predictions_RC, predictions_NGRC, predictions_hyb = hybrid_rc_ng
 t_pred /= hybrid_rc_ngrc.dyn_systems.lorenz_lyap_time
 
 hybrid_rc_ngrc.plotting.representative_trajectories(t_pred, actual, predictions_RC, predictions_NGRC, predictions_hyb, 
-                                          fname='2a.png')
+                                          fname='example_trajectories.png')
 hybrid_rc_ngrc.plotting.phase_space_trajectories_3d(actual, predictions_RC, predictions_NGRC, predictions_hyb,
-                                          fname='3a.png')
+                                          fname='phase_space_trajectories.png')
 hybrid_rc_ngrc.plotting.power_spectra_trajectories(actual, predictions_RC, predictions_NGRC, predictions_hyb,
-                                         fname='3b.png')
+                                         fname='power_spectra.png')
